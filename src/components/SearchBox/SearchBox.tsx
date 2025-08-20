800978fada847bc2fb64baa90d84259433e00cb9
karenkarap <myspecspace@gmail.com>
2025-08-20 11:55:20 +0400

    fix: debounce moved to App

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,19 +1,14 @@
-import { useDebouncedCallback } from 'use-debounce';
 import css from './SearchBox.module.css';
-import React from 'react';
+
 
 interface SearchBoxProps {
   setNoteQuery: (query: string) => void;
 }
 
 function SearchBox({ setNoteQuery }: SearchBoxProps) {
-  const handleSearchQuery = useDebouncedCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => setNoteQuery(e.target.value.trim()),
-    300
-  );
   return (
     <input
-      onChange={handleSearchQuery}
+      onChange={(e) => setNoteQuery(e.target.value.trim())}
       className={css.input}
       type="text"
       placeholder="Search notes"
